27afb9fba4eb66386b7d452e3b383046616df1e5
mojtaba <darzimojtaba@gmail.com>
2025-02-17 22:47:31 +0330

    feat(utils): Add utility functions

---
+++ src/utils/truncate.ts
@@ -0,0 +1,13 @@
+export const truncate = (text: string, maxLength: number, suffix = "...") => {
+    if (maxLength < 0) maxLength = Math.abs(maxLength)
+    if (text.length <= maxLength) {
+        return text;
+    }
+
+    const trimmedLength = maxLength - suffix.length;
+    if (trimmedLength <= 0) {
+        return suffix.slice(0, maxLength);
+    }
+
+    return text.slice(0, trimmedLength) + suffix;
+}
